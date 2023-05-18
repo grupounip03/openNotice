fd1c3c33a4ac84fc10b4149b7f0139ef70684eb8
grupounip03 <126683125+grupounip03@users.noreply.github.com>
2023-05-18 14:32:05 -0300

    Delete openNotice-main directory

--- openNotice-main/scripts/imagepsycho-log.js
+++
@@ -1,8 +0,0 @@
-function showImage() {
-    document.getElementById("image-container").style.display = "block";
-  }
-  
-  function hideImage() {
-    document.getElementById("image-container").style.display = "none";
-  }
-  
